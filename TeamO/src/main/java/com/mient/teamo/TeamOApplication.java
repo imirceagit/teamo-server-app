0814346828f3e8e311d3e03b5be9e915c74add2b
Mircea Ionita <mircea.ionita@hotmail.com>
2017-06-01 22:53:42 +0300

    Sts SpringBoot generated

---
+++ TeamO/src/main/java/com/mient/teamo/TeamOApplication.java
@@ -0,0 +1,12 @@
+package com.mient.teamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOApplication.class, args);
+	}
+}
---
+++ TeamO/src/test/java/com/mient/teamo/TeamOApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mient.teamo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamOApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
